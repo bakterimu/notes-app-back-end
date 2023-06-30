2c04c45ec41be6c460b366237fd7365f94780a43
Izhar Mayranda <mayrandai1290@gmail.com>
2023-06-30 21:24:26 +0700

    onPreResponse event extension implemented

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/clientError');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -26,6 +27,37 @@ const init = async () => {
     },
   });
 
+  server.ext('onPreResponse', (request, h) => {
+    // konteks response dari request
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // penangan client error secara internal
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      // penangan client error dari native hapi seperti 404
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    // jika bukan error maka lanjut
+    return h.continue;
+  });
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
