667666b752345a172246303041711b0647337d85
Izhar Mayranda <mayrandai1290@gmail.com>
2023-06-30 21:37:20 +0700

    autoBind handler implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-underscore-dangle */
-
+const autoBind = require('auto-bind');
 /* eslint-disable no-unused-vars */
 class NotesHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
 
-    this.postNoteHandler = this.postNoteHandler.bind(this);
-    this.getNotesHandler = this.getNotesHandler.bind(this);
-    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
-    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
-    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+    autoBind(this);
   }
 
   async postNoteHandler(request, h) {
